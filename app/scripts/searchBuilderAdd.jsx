0245d446e0c891e95c11f27ab19716006a065319
Rich Trott <rtrott@gmail.com>
2014-04-25 14:43:52 -0700

    only highlight toggle button

--- app/scripts/searchBuilderAdd.jsx
+++ app/scripts/searchBuilderAdd.jsx
@@ -30,13 +30,15 @@ var app = app || {};
         },
         render: function () {
             var revisePulldownStyle = {display: "inherit"};
+            var toggleClass = "";
             if (this.state.plusOrMinus === "plus") {
                 revisePulldownStyle = {display: "none"}
+                toggleClass = "dropdown-toggle"
             }
             return (
                 <div className="input-group-btn">
                     <button style={revisePulldownStyle} type="button" className="btn btn-default dropdown-toggle" data-toggle="dropdown">{this.state.boolean} <span className="caret"></span></button>
-                    <button type="button" onClick={this.remove} className="btn btn-default dropdown-toggle" data-toggle="dropdown"><span className={"glyphicon glyphicon-" + this.state.plusOrMinus}></span></button>
+                    <button type="button" onClick={this.remove} className="btn btn-default {toggleClass}" data-toggle="dropdown"><span className={"glyphicon glyphicon-" + this.state.plusOrMinus}></span></button>
                     <ul className="dropdown-menu dropdown-menu-right" role="menu">
                         <li><a data-boolean="AND" onClick={this.add} href="#">AND</a></li>
                         <li><a data-boolean="OR" onClick={this.add} href="#">OR</a></li>
