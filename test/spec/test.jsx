1e6801053d86ad1de8a583faa5d996a97e3309ca
Rich Trott <rtrott@gmail.com>
2014-05-02 16:47:50 -0700

    GithubRibbon test

--- test/spec/test.jsx
+++ test/spec/test.jsx
@@ -1,12 +1,26 @@
-/*global describe, it, expect, jasmine */
+/**
+ * @jsx React.DOM
+ */
 'use strict';
 
 var React = require('react/addons');
+var ReactTestUtils = React.addons.TestUtils;
 
 (function () {
     describe('React.addons', function () {
         it('should include TestUtils', function () {
-            expect(React.addons.TestUtils).toEqual(jasmine.any(Object));
+            expect(ReactTestUtils).toEqual(jasmine.any(Object));
+        });
+    });
+
+    describe('LTDL3', function () {
+        describe('GithubRibbon', function () {
+            var GithubRibbon = require('../../app/scripts/GithubRibbon.jsx');
+
+            it('should render a link to the GitHub repo', function () {
+                var ribbon = ReactTestUtils.renderIntoDocument(<GithubRibbon/>);
+                expect(ribbon.getDOMNode().getAttribute('href')).toBe('https://github.com/Trott/ltdl3-client');
+            });
         });
     });
 })();
