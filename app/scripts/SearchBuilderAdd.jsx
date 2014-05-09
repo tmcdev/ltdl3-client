43b0a236ea86338c9800f2b9322f49f8c9de1dc7
Rich Trott <rtrott@gmail.com>
2014-05-09 09:31:19 -0700

    refactor

--- app/scripts/SearchBuilderAdd.jsx
+++ app/scripts/SearchBuilderAdd.jsx
@@ -3,46 +3,44 @@
  */
 var React = require('react');
 
-// TODO: magic value plusOrMinus = "plus" or "minus" should be booleanized or enumed
-
 (function () {
     'use strict';
 
     module.exports = React.createClass({
         getInitialState: function () {
             return {
-                plusOrMinus: "plus",
-                boolean: ""
+                isAdd: true,
+                label: ""
             }
         },
         add: function (event) {
-            this.setState({boolean: event.target.getAttribute('data-boolean')});
+            this.setState({label: event.target.getAttribute('data-label')});
 
-            if (this.state.plusOrMinus === "plus") {
-                this.setState({plusOrMinus: "minus"});
+            if (this.state.isAdd) {
+                this.setState({isAdd: false});
                 this.props.add(this.props.index);
             }
         },
         remove: function (event) {
-            if (this.state.plusOrMinus === "minus") {
+            if (! this.state.isAdd) {
                 this.props.remove(this.props.index);
             }
         },
         render: function () {
             var revisePulldownStyle = {display: "inherit"};
             var toggleClass = "";
-            if (this.state.plusOrMinus === "plus") {
+            if (this.state.isAdd) {
                 revisePulldownStyle = {display: "none"}
                 toggleClass = "dropdown-toggle"
             }
             return (
                 <div className="input-group-btn">
-                    <button style={revisePulldownStyle} type="button" className="btn btn-default dropdown-toggle" data-toggle="dropdown">{this.state.boolean} <span className="caret"></span></button>
-                    <button type="button" onClick={this.remove} className="btn btn-default {toggleClass}" data-toggle="dropdown"><span className={"glyphicon glyphicon-" + this.state.plusOrMinus}></span></button>
+                    <button style={revisePulldownStyle} type="button" className="btn btn-default dropdown-toggle" data-toggle="dropdown">{this.state.label} <span className="caret"></span></button>
+                    <button type="button" onClick={this.remove} className="btn btn-default {toggleClass}" data-toggle="dropdown"><span className={"glyphicon glyphicon-" + (this.state.isAdd ? "plus" : "minus")}></span></button>
                     <ul className="dropdown-menu dropdown-menu-right" role="menu">
-                        <li><a data-boolean="AND" onClick={this.add} href="#">AND</a></li>
-                        <li><a data-boolean="OR" onClick={this.add} href="#">OR</a></li>
-                        <li><a data-boolean="NOT" onClick={this.add} href="#">NOT</a></li>
+                        <li><a data-label="AND" onClick={this.add} href="#">AND</a></li>
+                        <li><a data-label="OR" onClick={this.add} href="#">OR</a></li>
+                        <li><a data-label="NOT" onClick={this.add} href="#">NOT</a></li>
                     </ul>
                 </div>
             )
